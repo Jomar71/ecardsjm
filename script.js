7d6267d79c593b4fea2c72ebb9cf0141c588aace
George <jorgenriquemartinezc@gmail.com>
2026-03-20 18:54:20 -0500

    solucion para que los dispositivos moviles miren las tarjetas

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const state = {
     bgImagePath: null,
     fontFilePath: null,
     archives: [],
-    API_BASE: (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.hostname.startsWith('192.168.')) 
+    API_BASE: (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.hostname.startsWith('192.168.') || window.location.hostname.startsWith('10.') || window.location.hostname.match(/\d+\.\d+\.\d+\.\d+/)) 
         ? `http://${window.location.hostname}:3000` 
         : window.location.origin
 };
