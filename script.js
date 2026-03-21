6271259587be9be9d72c9d0a6b654d4fa3823d86
George <jorgenriquemartinezc@gmail.com>
2026-03-20 21:27:21 -0500

    esperemos que ya solucionemos todo

--- script.js
+++ script.js
@@ -17,13 +17,14 @@ const state = {
     archives: [],
     API_BASE: (() => {
         // Detectar si estamos en un entorno local o en producción
-        if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || 
-            window.location.hostname.match(/^192\.168\./) || 
-            window.location.hostname.match(/^10\./) || 
-            window.location.hostname.match(/^172\.(1[6-9]|2[0-9]|3[01])\./) ||
-            window.location.hostname.match(/\d+\.\d+\.\d+\.\d+/)) {
+        const hostname = window.location.hostname;
+        if (hostname === 'localhost' || hostname === '127.0.0.1' || 
+            hostname.match(/^192\.168\./) || 
+            hostname.match(/^10\./) || 
+            hostname.match(/^172\.(1[6-9]|2[0-9]|3[01])\./) ||
+            hostname.match(/\d+\.\d+\.\d+\.\d+/)) {
             // Si es una IP local o privada, usar el puerto 3000
-            return `http://${window.location.hostname}:3000`;
+            return `http://${hostname}:3000`;
         } else {
             // De lo contrario, usar el origen actual
             return window.location.origin;
